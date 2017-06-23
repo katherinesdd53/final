e688ec0ec800e4d796bf0f1119240cca41faacef
katherinesdd53 <katherinesdd53@gmail.com>
2017-06-23 01:03:02 -0400

    Basic Website Skeleton

--- index.js
+++ index.js
@@ -1,52 +1,23 @@
 var express = require('express');
 var app = express();
-// const bodyParser = require('body-parser');
-const pg = require('pg');
-// const conString = 'postgres://postgres@db-endpoint:5432/postgres';
 
 app.set('views', __dirname + '/views');
 app.set('view engine','ejs');
 
-// app.use(bodyParser());
-// app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static("public"));
 
-app.get('/', function(req, res){
-	res.render('home.ejs',{
-		name: "Home Page"
-	})
+app.get('/home', function(req, res){
+	res.render('homepage.ejs')
+});
+
+app.get('/blog', function(req, res){
+	res.render('blog.ejs')
 })
 
-app.get('/post', function (req, res, next) {
-	pg.connect(conString, function (err, client, done){
-    if (err){
-      return next(err)
-    }
-    client.query('SELECT * FROM posts;', function (err, result){
-      if (err){
-        return next(err)
-      }
-			res.render('blogpost.ejs',{posts:result.rows})
-      done()
-    })
-  })
-})
-
-app.post('/post',function(req,res,next){
-  const blogPost = req.body
-  pg.connect(conString, function (err, client, done){
-    if (err){
-      return next(err)
-    }
-    client.query('INSERT INTO posts (title,body) VALUES ($1, $2);',[blogPost.title,blogPost.text], function (err, result){
-      done()
-      if (err){
-        return next(err)
-      }
-      res.redirect('/post')
-    })
-  })
+app.get('/contact', function(req, res){
+	res.render('contact.ejs')
 })
 
 app.listen(process.env.PORT || 3000, function (){
 	console.log('Example app listening on port 3000!')
-})
+});
